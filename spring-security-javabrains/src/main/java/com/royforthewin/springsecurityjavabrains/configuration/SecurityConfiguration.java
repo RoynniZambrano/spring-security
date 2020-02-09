2640088102e3615fea8cb15f9ac0fed36c65b6f0
Roy <roynny51@gmail.com>
2020-02-09 17:04:05 -0300

    authentication with jdbc api

--- spring-security-javabrains/src/main/java/com/royforthewin/springsecurityjavabrains/configuration/SecurityConfiguration.java
+++ spring-security-javabrains/src/main/java/com/royforthewin/springsecurityjavabrains/configuration/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.royforthewin.springsecurityjavabrains.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,13 +9,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
+        auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .usersByUsernameQuery(" select username, password, enabled " +
+                " from users " +
+                " where username = ? ")
+        .authoritiesByUsernameQuery(" select username, authority " +
+                " from authorities " +
+                " where username = ?")
+        ;
+
+
+        /* // in memory Authentication
         auth.inMemoryAuthentication()
                 .withUser("user")
                 .password("user")
@@ -23,6 +41,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .withUser("foo")
                 .password("foo")
                 .roles("ADMIN");
+
+        */
 
     }
 
