c07dfb863058d50b67e338c62cfd6faa76f85297
Roy <roynny51@gmail.com>
2020-02-16 20:31:09 -0300

    jwt

---
+++ spring-security-jwt/src/main/java/com/royforthewin/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.royforthewin.springsecurityjwt.models;
+
+
+import lombok.Getter;
+
+@Getter
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+}
